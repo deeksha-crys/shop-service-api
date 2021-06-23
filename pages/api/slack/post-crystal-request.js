79e4f0f0094fdc34b7071ac10cdaa03e54c82b6f
deeksha-crys <deeksha@crystallize.com>
2021-06-23 12:31:14 -0700

    updated response for post-crystal-request

--- pages/api/slack/post-crystal-request.js
+++ pages/api/slack/post-crystal-request.js
@@ -2,8 +2,11 @@ import cors from "../../../lib/cors";
 import { postToSlack } from "../../../src/services/slack/send-crystal-req";
 
 async function PostCrystalRequest(req, res) {
+  if (Object.keys(req.body).length === 0)
+    res.status(400).send({ message: "Invalid data in request body" });
   postToSlack({ ...req.body }).then((response) => {
-    if (response.status === 200) res.send({ message: "success" });
+    if (response.status === 200) res.send({ message: "Success" });
+    else res.send({ message: "Failed to send message" });
   });
 }
 
