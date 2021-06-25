5742a3c9d53355ee536ce1271794ae3ef6a7036e
deeksha-crys <deeksha@crystallize.com>
2021-06-25 09:29:27 -0700

    Check paymentMethod before sending invoice

--- src/services/payment-providers/stripe/generate-invoice-and-charge.js
+++ src/services/payment-providers/stripe/generate-invoice-and-charge.js
@@ -9,58 +9,61 @@ module.exports = async function generateInvoiceAndChargePayment(
     type: "card",
   });
   const stripeCustomer = await getClient().customers.retrieve(customerId);
-  const paymentMethodId = paymentMethods.data[0]["id"];
+  const paymentMethodId =
+    paymentMethods?.data.length > 0 ? paymentMethods?.data[0]["id"] : null;
   const customerTenantId = stripeCustomer?.metadata?.customerTenantId;
 
-  await getClient().invoiceItems.create({
-    customer: customerId,
-    unit_amount: usage.orders.unit_amount,
-    quantity: usage.orders.quantity,
-    description: "Extra Orders",
-    currency: "usd",
-  });
-  await getClient().invoiceItems.create({
-    customer: customerId,
-    unit_amount: usage.items.unit_amount,
-    quantity: usage.items.quantity,
-    description: "Extra Items",
-    currency: "usd",
-  });
+  if (paymentMethodId) {
+    await getClient().invoiceItems.create({
+      customer: customerId,
+      unit_amount: usage.orders.unit_amount,
+      quantity: usage.orders.quantity,
+      description: "Extra Orders",
+      currency: "usd",
+    });
+    await getClient().invoiceItems.create({
+      customer: customerId,
+      unit_amount: usage.items.unit_amount,
+      quantity: usage.items.quantity,
+      description: "Extra Items",
+      currency: "usd",
+    });
 
-  await getClient().invoiceItems.create({
-    customer: customerId,
-    unit_amount: usage.bandwidth.unit_amount,
-    quantity: usage.bandwidth.quantity,
-    description: "Extra Bandwidth",
-    currency: "usd",
-  });
+    await getClient().invoiceItems.create({
+      customer: customerId,
+      unit_amount: usage.bandwidth.unit_amount,
+      quantity: usage.bandwidth.quantity,
+      description: "Extra Bandwidth",
+      currency: "usd",
+    });
 
-  await getClient().invoiceItems.create({
-    customer: customerId,
-    unit_amount: usage.apiCalls.unit_amount,
-    quantity: usage.apiCalls.quantity,
-    description: "Extra Api Calls",
-    currency: "usd",
-  });
+    await getClient().invoiceItems.create({
+      customer: customerId,
+      unit_amount: usage.apiCalls.unit_amount,
+      quantity: usage.apiCalls.quantity,
+      description: "Extra Api Calls",
+      currency: "usd",
+    });
 
-  await getClient().invoiceItems.create({
-    customer: customerId,
-    unit_amount: 1,
-    quantity: 0,
-    description: "Flat fee",
-    currency: "usd",
-  });
+    await getClient().invoiceItems.create({
+      customer: customerId,
+      unit_amount: 1,
+      quantity: 0,
+      description: "Flat fee",
+      currency: "usd",
+    });
 
-  const invoice = await getClient().invoices.create({
-    customer: customerId,
-    default_tax_rates: [taxRateId],
-    metadata: { customerTenantId: customerTenantId },
-  });
-  const finalizedInvoice = await getClient().invoices.finalizeInvoice(
-    invoice.id,
-    { auto_advance: true }
-  );
-  return await getClient().invoices.pay(finalizedInvoice.id, {
-    payment_method: paymentMethodId,
-  });
+    const invoice = await getClient().invoices.create({
+      customer: customerId,
+      default_tax_rates: [taxRateId],
+      metadata: { customerTenantId: customerTenantId },
+    });
+    const finalizedInvoice = await getClient().invoices.finalizeInvoice(
+      invoice.id,
+      { auto_advance: true }
+    );
+    return await getClient().invoices.pay(finalizedInvoice.id, {
+      payment_method: paymentMethodId,
+    });
+  }
 };
