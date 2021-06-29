41a4eba5414afd396665316c45196164c42fdef6
deeksha-crys <deeksha@crystallize.com>
2021-06-29 09:36:49 -0700

    stripe webhook calls payment-success

--- pages/api/crystallize/payment-success.js
+++ pages/api/crystallize/payment-success.js
@@ -1,26 +1,13 @@
 import cors from "../../../lib/cors";
-// import { informPaymentFailedToCrystallize } from "../../../src/services/slack/send-payment-failed";
-// import setPipelineStage from "../../../src/services/crystallize/orders/set-pipeline-stage";
+import setPipelineStage from "../../../src/services/crystallize/orders/set-pipeline-stage";
 
 async function InvoicePaymentSuccess(req, res) {
-  console.log("Invoice payment success -> ", req.body);
-  // const {
-  //   customer,
-  //   customer_email,
-  //   customer_name,
-  //   subtotal,
-  //   tax,
-  //   total,
-  //   hosted_invoice_url,
-  //   invoice_pdf,
-  // } = {
-  //   ...req.body,
-  // };
-  // const setPipelineStageResponse = await setPipelineStage({
-  //   orderId: "orderId",
-  //   stageName: "success",
-  // });
-
+  const orderId = req.body.data.object.metadata.crystallizeOrderId;
+  const setPipelineStageResponse = await setPipelineStage({
+    orderId: orderId,
+    stageName: "success",
+  });
+  console.log("setPipelineStageResponse -> ", setPipelineStageResponse);
   res.status(200).send({ message: "Invoice is paid" });
 }
 
